27b19c581a10ec7c69985443df551f71a3ca264a
Bijoy Paul <bijoy.paul@tothenew.com>
2016-06-24 00:04:42 +0530

    initial setup

--- src/main/java/instance/ClientInstance.java
+++ src/main/java/instance/ClientInstance.java
@@ -8,7 +8,25 @@ import com.hazelcast.core.HazelcastInstance;
  * Created by bijoy on 16/6/16.
  */
 public class ClientInstance{
+    private static ClientConfig clientConfig ;
+    private static HazelcastInstance hazelcastInstance ;
+
+    static {
+        clientConfig = clientConfig();
+    }
+
     public static HazelcastInstance instance(){
+        while (hazelcastInstance == null || !hazelcastInstance.getLifecycleService().isRunning()){
+            try{
+                hazelcastInstance = HazelcastClient.newHazelcastClient(clientConfig);
+            }catch (Exception ex){
+                System.out.println("Exception "+ex.getMessage());
+            }
+        }
+        return hazelcastInstance;
+    }
+
+    private static ClientConfig clientConfig(){
         ClientConfig clientConfig = new ClientConfig();
         clientConfig.getGroupConfig()
                 .setName("intellimeet")
@@ -18,8 +36,8 @@ public class ClientInstance{
                 .addAddress("192.168.1.102")
                 .setConnectionAttemptLimit(5) // by default 2
                 .setConnectionAttemptPeriod(1000) // by default 3000 ms
-                .setConnectionTimeout(3000); // by default 5000 ms
-        return HazelcastClient.newHazelcastClient(clientConfig);
+                .setConnectionTimeout(1000); // by default 5000 ms
+        return clientConfig;
     }
 
 }
