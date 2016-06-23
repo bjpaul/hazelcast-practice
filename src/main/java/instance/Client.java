5b5b0cf7396666b30b98a77d00f0085f4af3fe58
Bijoy Paul <bijoy.paul@tothenew.com>
2016-06-23 23:41:36 +0530

    initial setup

--- src/main/java/instance/Client.java
+++ src/main/java/instance/Client.java
@@ -1,13 +1,24 @@
 package instance;
 
+
 import com.hazelcast.client.HazelcastClient;
+import com.hazelcast.client.config.ClientConfig;
 import com.hazelcast.core.HazelcastInstance;
 
 /**
- * Created by bijoy on 23/6/16.
+ * Created by bijoy on 15/6/16.
  */
 public class Client {
-    public static HazelcastInstance instance(){
-        return HazelcastClient.newHazelcastClient();
+    public static void main(String[] args){
+        ClientConfig clientConfig = new ClientConfig();
+        HazelcastInstance hazelcastInstance = HazelcastClient.newHazelcastClient(clientConfig);
+        while (true){
+            try {
+                 System.out.println("Receive ==>> " + hazelcastInstance.getQueue("testQueue").take());
+            } catch (Exception e) {
+                e.getMessage();
+            }
+        }
     }
+
 }
