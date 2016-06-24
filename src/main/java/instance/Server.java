d91733ec6ab56df672243deb6818637538ad45dd
Bijoy Paul <bijoy.paul@tothenew.com>
2016-06-24 18:44:35 +0530

    refactotred

--- src/main/java/instance/Server.java
+++ src/main/java/instance/Server.java
@@ -1,54 +1,17 @@
 package instance;
 
-import com.hazelcast.config.Config;
-import com.hazelcast.core.Cluster;
 import com.hazelcast.core.Hazelcast;
 import com.hazelcast.core.HazelcastInstance;
-import com.hazelcast.core.Member;
-import com.hazelcast.nio.Address;
 import listners.CustomMembershipListner;
-
-import java.util.Set;
-
 /**
  * Created by bijoy on 23/6/16.
  */
 public class Server {
 
     public static HazelcastInstance instance(){
-        Config config = new Config();
-        config.getMemberAttributeConfig().setStringAttribute("user", System.getProperty("user.name"));
-        HazelcastInstance hazelcastInstance = Hazelcast.newHazelcastInstance(config);
+        HazelcastInstance hazelcastInstance = Hazelcast.newHazelcastInstance();
         hazelcastInstance.getCluster().addMembershipListener(new CustomMembershipListner());
-        membersString(hazelcastInstance.getCluster());
         return hazelcastInstance;
     }
 
-    private static void membersString(Cluster cluster){
-        Set<Member> members = cluster.getMembers();
-        Address address;
-        System.out.println("Members [" + members.size() + "] {");
-        StringBuilder sb ;
-        for (Member member : members) {
-            sb= new StringBuilder();
-            address = member.getAddress();
-            sb.append("    [");
-            sb.append(member.getStringAttribute("user"));
-            sb.append(" -> Instance ");
-            sb.append(address.getPort() % 10);
-            sb.append("] [");
-            sb.append(address.getHost());
-            sb.append("]");
-            sb.append(":");
-            sb.append(address.getPort());
-            if (member.localMember()) {
-                sb.append(" this");
-            }
-            if (member.isLiteMember()) {
-                sb.append(" lite");
-            }
-            System.out.println(sb);
-        }
-        System.out.println("}");
-    }
 }
--- src/main/java/listners/CustomMembershipListner.java
+++ src/main/java/listners/CustomMembershipListner.java
@@ -12,46 +12,16 @@ public class CustomMembershipListner implements MembershipListener {
     @Override
     public void memberAdded(MembershipEvent membershipEvent) {
         System.out.println("#####################"+ membershipEvent);
-        membersString(membershipEvent.getCluster());
     }
 
     @Override
     public void memberRemoved(MembershipEvent membershipEvent) {
         System.out.println("#####################"+ membershipEvent);
-        membersString(membershipEvent.getCluster());
     }
 
     @Override
     public void memberAttributeChanged(MemberAttributeEvent memberAttributeEvent) {
         System.out.println("##################### Member "+ memberAttributeEvent.getMember().getAddress()+", attribute "+memberAttributeEvent.getValue());
-        membersString(memberAttributeEvent.getCluster());
     }
 
-    private void membersString(Cluster cluster){
-        Set<Member> members = cluster.getMembers();
-        Address address;
-        System.out.println("Members [" + members.size() + "] {");
-        StringBuilder sb ;
-        for (Member member : members) {
-            sb= new StringBuilder();
-            address = member.getAddress();
-            sb.append("    [");
-            sb.append(member.getStringAttribute("user"));
-            sb.append(" -> Instance ");
-            sb.append(address.getPort() % 10);
-            sb.append("] [");
-            sb.append(address.getHost());
-            sb.append("]");
-            sb.append(":");
-            sb.append(address.getPort());
-            if (member.localMember()) {
-                sb.append(" this");
-            }
-            if (member.isLiteMember()) {
-                sb.append(" lite");
-            }
-            System.out.println(sb);
-        }
-        System.out.println("}");
-    }
 }
